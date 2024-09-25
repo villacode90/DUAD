a7d43af1e4deca0b6404c803ab7ce01e1e9658bd
Fabian Villa <villa.code90@gmail.com>
2024-09-24 18:36:55 -0600

    Delete Semana 10 Folder

--- Semana 10/Modulos/Data/Data.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def read_csv_file(file_path):
-  with open(file_path, 'r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-      print(row)
-
-read_csv_file("./Data.csv")
